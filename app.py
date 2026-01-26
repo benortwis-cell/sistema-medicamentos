e39683a87d2870891b997220e6d075d87b8e3a25
Olger B <benortwis@gmail.com>
2026-01-26 15:08:46 -0500

    Registro y visualización básica de medicamentos completada

--- app.py
+++ app.py
@@ -23,21 +23,40 @@ def home():
     # Enviamos la lista al HTML
     return render_template('index.html', medicamentos=lista_medicamentos)
 
+@app.route('/eliminar/<int:id>')
+def eliminar(id):
+    conexion = conectar_db()
+    cursor = conexion.cursor()
+    
+    # SQL para eliminar por ID
+    cursor.execute('DELETE FROM medicamentos WHERE id = ?', (id,))
+    
+    conexion.commit()
+    conexion.close()
+    return redirect('/')
+
 @app.route('/agregar', methods=['POST'])
 def agregar():
     nombre = request.form['nombre']
+    lote = request.form['lote']
+    cantidad = request.form['cantidad']
+    vencimiento = request.form['vencimiento'] # Fecha del HTML
     precio = request.form['precio']
 
     conexion = conectar_db()
     cursor = conexion.cursor()
-    # Creamos la tabla si no existe (SQL básico)
-    cursor.execute('CREATE TABLE IF NOT EXISTS medicamentos (id INTEGER PRIMARY KEY, nombre TEXT, precio REAL)')
-    # Insertamos los datos
-    cursor.execute('INSERT INTO medicamentos (nombre, precio) VALUES (?, ?)', (nombre, precio))
+    
+    # 1. Insertar o buscar el medicamento
+    cursor.execute('INSERT OR IGNORE INTO medicamentos (nombre) VALUES (?)', (nombre,))
+    
+    # 2. Insertar el lote con su fecha
+    cursor.execute('''
+        INSERT INTO lotes (nombre_med, codigo_lote, stock, fecha_vence, precio) 
+        VALUES (?, ?, ?, ?, ?)''', 
+        (nombre, lote, cantidad, vencimiento, precio))
     
     conexion.commit()
     conexion.close()
-    
     return redirect('/')
 
 if __name__ == '__main__':
